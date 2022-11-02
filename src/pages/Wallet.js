9e1a3c7501b2027c9cd361bfd18f6452bb5f5bba
Joseane Oliveira <joseaneoliveira.joli@gmail.com>
2022-11-02 14:28:30 -0300

    Requisito 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from '../components/Header';
+import Table from '../components/Table';
 import WalletForm from '../components/WalletForm';
 
 class Wallet extends React.Component {
   render() {
     return (
-      <Header />
-      <WalletForm />
+      <>
+        <Header />
+        <WalletForm />
+        <Table />
+      </>
     );
   }
 }
